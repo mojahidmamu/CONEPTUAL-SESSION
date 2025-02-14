52a2a12d33ac9eba942094f82641011a6434c8e1
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-02-14 15:10:45 +0600

    added mojahid

--- Milestone-7/React JS Day/app.jsx
+++ Milestone-7/React JS Day/app.jsx
@@ -1,18 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const app = () => {
-    const handleHeroAlom = (message) => {
-        console.log(message)
-    }
-    return (
-        <div>
-            <h1>this is Mojahid...</h1>
-            <h1>this is jsx file...</h1>
+  const handleHeroAlom = (message) => {
+    console.log(message);
+  };
+  return (
+    <div>
+      <h1>this is Mojahid...</h1>
+      <h1>this is jsx file...</h1>
 
-
-            <button onClick={handleHeroAlom}>Just Hero Alom</button>
-        </div>
-    );
+      <button onClick={() => handleHeroAlom("mojahid")}>Just Hero Alom</button>
+    </div>
+  );
 };
 
 export default app;
