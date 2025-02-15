5b2039e6475db442a97d9bddf6a8c3be8440605f
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-02-15 10:05:33 +0600

    added

--- Milestone-7/React-Project/name-of-your-project/src/App.jsx
+++ Milestone-7/React-Project/name-of-your-project/src/App.jsx
@@ -1,16 +1,39 @@
+import { useState } from "react";
 import "./App.css";
 import AllProducts from "./component/AllProducts/AllProducts";
 import CardContainer from "./component/CardContainer/CardContainer";
 import Navbar from "./component/Navbar/Navbar";
 
 function App() {
+  const [isActive, setIsActive] = useState({
+    card: true,
+    status: "cart",
+  });
+
+  const handelIsActive = (status) => {
+    if (status == "cart") {
+      setIsActive({
+        card: true,
+        status: "cart",
+      });
+    } 
+    else {
+      setIsActive({
+        card: false,
+        status: "about",
+      });
+    }
+  };
+
+  console.log(isActive);
+
   return (
     <>
-      <Navbar></Navbar>
+      <Navbar />
 
       <div className="flex justify-around">
         <AllProducts></AllProducts>
-        <CardContainer></CardContainer>
+        <CardContainer isActive={isActive} handelIsActive={handelIsActive}></CardContainer>
       </div>
     </>
   );
--- Milestone-7/React-Project/name-of-your-project/src/component/CardContainer/CardContainer.jsx
+++ Milestone-7/React-Project/name-of-your-project/src/component/CardContainer/CardContainer.jsx
@@ -1,16 +1,32 @@
-import React from 'react';
-import './CardContainer.css'
+import React from "react";
+import "./CardContainer.css";
+import Card from "../Card/Card";
+import About from "../About/About";
 
-const CardContainer = () => {
-    return (
-        <div>
-            <h1 className='text-3xl font-bold'> CardContainer</h1>
-            <div className='flex justify-around'>
-                <div>Cart</div>
-                <div>About</div>
-            </div>
+const CardContainer = ({ handelIsActive, isActive }) => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold"> CardContainer</h1>
+      <div className="flex justify-around">
+        <div
+          onClick={() => handelIsActive("cart")}
+          className={`${isActive.card ? "btn active" : "btn"}`}
+        >
+          Cart
         </div>
-    );
+        <div
+          onClick={() => handelIsActive("about")}
+          className={`${isActive.card ? "btn" : "btn active"}`}
+        >
+          About
+        </div>
+      </div>
+
+      
+
+      {isActive.card ? <Card></Card> : <About></About>}
+    </div>
+  );
 };
 
 export default CardContainer;
