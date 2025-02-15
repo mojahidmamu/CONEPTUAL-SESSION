1aa924c3e352b4171bd18a68e8bf805f01a6e43a
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-02-15 08:47:57 +0600

    added new file and folder

---
+++ Milestone-7/React-Project/name-of-your-project/src/component/Card/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+          <h1>this is card.jsx</h1>  
+        </div>
+    );
+};
+
+export default Card;<h1>this is card.jsx</h1>
