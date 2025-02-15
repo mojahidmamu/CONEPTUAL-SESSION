a5f3f0e630bca86e405da908d44ee055b06254ad
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-02-15 10:25:11 +0600

    added

--- Milestone-7/React-Project/name-of-your-project/src/component/AllProducts/AllProducts.jsx
+++ Milestone-7/React-Project/name-of-your-project/src/component/AllProducts/AllProducts.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './AllProducts.css'
 
 const AllProducts = () => {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect( () => {
+        fetch("../../../public/fakeData.json()")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+        .catch(error => console.error('error fetching data', error));
+    }, [])
+
     return (
         <div>
              <h1 className='text-3xl font-bold'>All product</h1>
