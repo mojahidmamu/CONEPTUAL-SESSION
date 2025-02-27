490d23dd9752bee54a8e8e7d5b4b0a7a44dfc551
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-02-27 10:01:18 +0600

    added new file and folder

---
+++ Milestone-9/Firebase-Conceptual-01/src/components/MainLayout/MainLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainLayout = () => {
+    return (
+        <div>
+            <h1>I am in MainLayout..</h1>
+        </div>
+    );
+};
+
+export default MainLayout;<h1>I am in MainLayout..</h1>
