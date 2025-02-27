73f265b8e2a1a5073538a310701ed651056f102b
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-02-27 12:18:22 +0600

    added new file and folder

--- Milestone-9/Firebase-Conceptual-01/src/components/About/About.jsx
+++ Milestone-9/Firebase-Conceptual-01/src/components/About/About.jsx
@@ -1,17 +1,17 @@
-import { GoogleAuthProvider , signInWithPopup, } from 'firebase/auth';
+// import { GoogleAuthProvider , signInWithPopup, } from 'firebase/auth';
 import React from 'react';
-import auth from '../Firebase/firebase.config';
+// import auth from '../Firebase/firebase.config';
 
 const About = () => {
-    const githubProvider = new GoogleAuthProvider();
-    const handleGithubLogin = () => {
-        signInWithPopup(auth, githubProvider)
-        .then(result => console.log(result))
-    }
+    // const githubProvider = new GoogleAuthProvider();
+    // const handleGithubLogin = () => {
+    //     signInWithPopup(auth, githubProvider)
+    //     .then(result => console.log(result))
+    // }
     return (
         <div>
            I am in about  <br />
-           <button onClick={handleGithubLogin} className='btn btn-outline my-5 mx-5'>Github Login</button>
+           {/* <button onClick={handleGithubLogin} className='btn btn-outline my-5 mx-5'>Github Login</button> */}
         </div>
     );
 };
--- Milestone-9/Firebase-Conceptual-01/src/components/Home/Home.jsx
+++ Milestone-9/Firebase-Conceptual-01/src/components/Home/Home.jsx
@@ -1,18 +1,24 @@
-import { GoogleAuthProvider, signInWithPopup, } from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup, } from "firebase/auth";
 import React from "react";
 import { Link } from "react-router-dom";
 import auth from "../Firebase/firebase.config";
 
 const Home = () => {
   const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
     const handleGoogleLogin = () => {
       signInWithPopup(auth, googleProvider)
+      .then(result => console.log(result))
+    }
+    const handleGithubLogin = () => {
+      signInWithPopup(auth, githubProvider)
       .then(result => console.log(result))
     }
   return (
     <div>
       <h1>I am in Home...</h1>
       <button  onClick={handleGoogleLogin} className="btn btn-secondary">Google Login</button> <br />
+      <button  onClick={handleGithubLogin} className="btn btn-outline my-5">Github Login</button> <br />
       
       <Link to="/about">
         <button className="btn btn-outline">Go to About</button>
