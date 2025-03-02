40c93efe1b07d88e285aec034578ee18a9e74feb
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-03-02 12:10:12 +0600

    added new file and folder

--- Milestone-9/Firebase-Conceptual-01/src/components/MainLayout/MainLayout.jsx
+++ Milestone-9/Firebase-Conceptual-01/src/components/MainLayout/MainLayout.jsx
@@ -1,4 +1,4 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 import { Outlet } from "react-router-dom";
 import auth from "../Firebase/firebase.config";
 import {
@@ -10,29 +10,31 @@ import {
 export const authContext = createContext();
 
 const MainLayout = () => {
+  const [user, setUser] = useState(null);
+
   const googleProvider = new GoogleAuthProvider();
   const githubProvider = new GithubAuthProvider();
   const twitterprovider = new TwitterAuthProvider();
   const handleGoogleLogin = () => {
-    signInWithPopup(auth, googleProvider).then((result) => console.log(result));
+    signInWithPopup(auth, googleProvider).then((result) => setUser(result.user));
   };
   const handleGithubLogin = () => {
-    signInWithPopup(auth, githubProvider).then((result) => console.log(result));
+    signInWithPopup(auth, githubProvider).then((result) => setUser(result.user));
   };
   const handleTwitterLogin = () => {
-    signInWithPopup(auth, twitterprovider).then((result) => console.log(result));
+    signInWithPopup(auth, twitterprovider).then((result) =>
+      setUser(result.user)    );
   };
 
-
   const authData = {
-   handleGithubLogin,
-   handleGoogleLogin, 
-   handleTwitterLogin,
-  }
+    handleGithubLogin,
+    handleGoogleLogin,
+    handleTwitterLogin,
+  };
   return (
     <div>
-      <authContext.Provider value={authData}>  
-        <Outlet></Outlet> 
+      <authContext.Provider value={authData}>
+        <Outlet></Outlet>
       </authContext.Provider>
     </div>
   );
