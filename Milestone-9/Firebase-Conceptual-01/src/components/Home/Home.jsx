8baa8350a85bab41f17ead61b2dda24561852506
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-03-02 10:46:16 +0600

    added new file and folder

--- Milestone-9/Firebase-Conceptual-01/src/components/Home/Home.jsx
+++ Milestone-9/Firebase-Conceptual-01/src/components/Home/Home.jsx
@@ -1,25 +1,40 @@
-import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup, } from "firebase/auth";
+import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  signInWithPopup,
+  TwitterAuthProvider,
+} from "firebase/auth";
 import React from "react";
 import { Link } from "react-router-dom";
 import auth from "../Firebase/firebase.config";
 
 const Home = () => {
+  const twitterprovider = new TwitterAuthProvider();
   const googleProvider = new GoogleAuthProvider();
   const githubProvider = new GithubAuthProvider();
-    const handleGoogleLogin = () => {
-      signInWithPopup(auth, googleProvider)
-      .then(result => console.log(result))
-    }
-    const handleGithubLogin = () => {
-      signInWithPopup(auth, githubProvider)
-      .then(result => console.log(result))
-    }
+  const handleGoogleLogin = () => {
+    signInWithPopup(auth, googleProvider).then((result) => console.log(result));
+  };
+  const handleGithubLogin = () => {
+    signInWithPopup(auth, githubProvider).then((result) => console.log(result));
+  };
+  const handleTwitterLogin = () => {
+    signInWithPopup(auth, twitterprovider).then((result) => console.log(result));
+  };
   return (
     <div>
       <h1>I am in Home...</h1>
-      <button  onClick={handleGoogleLogin} className="btn btn-secondary">Google Login</button> <br />
-      <button  onClick={handleGithubLogin} className="btn btn-outline my-5">Github Login</button> <br />
-      
+      <button onClick={handleGoogleLogin} className="btn btn-secondary">
+        Google Login
+      </button>{" "}
+      <br />
+      <button onClick={handleGithubLogin} className="btn btn-outline my-5">
+        Github Login
+      </button> <br />
+      <button onClick={handleTwitterLogin} className="btn btn-outline my-5">
+        Twitter Login
+      </button> <br />
+      <br />
       <Link to="/about">
         <button className="btn btn-outline">Go to About</button>
       </Link>
